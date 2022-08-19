f5fc65bec9c8aafd701b01a4b7a82b1e688c0fe7
Stan Schooten <stan.schooten@hotmail.com>
2022-08-19 13:05:16 +0200

    init

---
+++ backend/src/main/java/com/example/simplecalculator/BootListener.java
@@ -0,0 +1,27 @@
+package com.example.simplecalculator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@Component
+@Slf4j
+public class BootListener {
+    private final EntityManager entityManager;
+
+    public BootListener(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @EventListener
+    @Transactional
+    public void bootListener(ApplicationStartedEvent event) {
+        log.info("Creating DEVELOP seed data...");
+
+        log.info("Created DEVELOP seed data!");
+    }
+}
